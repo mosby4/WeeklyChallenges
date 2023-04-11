2d8b52cabfd530a544f6bc37d3375d254efe19a6
Mosby4 <rufus.hamby@gmial.com>
2023-04-11 11:33:26 -0500

    Week 3 started

--- ChallengesWithTestsMark8/ChallengesSet03.cs
+++ ChallengesWithTestsMark8/ChallengesSet03.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
@@ -7,12 +9,28 @@ namespace ChallengesWithTestsMark8
     {
         public bool ArrayContainsAFalse(bool[] vals)
         {
-            throw new NotImplementedException();
+            foreach (var item in vals)
+            {
+                if (item == false)
+                {
+                    return true;
+                }
+            }
+            return false;
         }
 
         public bool IsSumOfOddsOdd(IEnumerable<int> numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null)
+            {
+                return false;
+            }
+            if (numbers.Sum() % 2 == 0)
+            {
+                return false;
+            }
+            return true;
+
         }
 
         public bool PasswordContainsUpperLowerAndNumber(string password)
@@ -22,32 +40,45 @@ namespace ChallengesWithTestsMark8
 
         public char GetFirstLetterOfString(string val)
         {
-            throw new NotImplementedException();
+            return val[0];
         }
 
         public char GetLastLetterOfString(string val)
         {
-            throw new NotImplementedException();
+            return val.Last();
+
         }
 
         public decimal Divide(decimal dividend, decimal divisor)
         {
-            throw new NotImplementedException();
+            return dividend / divisor;
+
         }
 
         public int LastMinusFirst(int[] nums)
         {
-            throw new NotImplementedException();
+            return nums.Last() - nums[0];
+
         }
 
         public int[] GetOddsBelow100()
         {
+            /* var odds = new int[50];
+             if (% 2 == 1 < 100)
+             {
+                 odds.Add();
+
+             }*/
             throw new NotImplementedException();
         }
 
         public void ChangeAllElementsToUppercase(string[] words)
         {
-            throw new NotImplementedException();
+            foreach (var item in words)
+            {
+                item.ToUpper();
+            }
         }
+
     }
 }
